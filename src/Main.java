75ac8b7b66ab9f37a7e53339c1288964c425730f
zenitfan88 <nikanenkov@rambler.ru>
2022-06-11 13:51:27 +0400

    Added text

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         BodyMassIndex service = new BodyMassIndex();
         double index = service.calculate(78, 1.7);
-        System.out.printf("%.2f", index);
+        System.out.printf("Индекс массы тела - " + "%.2f", index);
     }
 }
